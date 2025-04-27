b615da9d332d089736f2dc2203bb41bd7039753b
acdevz <amanchandra.me@gmail.com>
2025-04-27 23:35:45 +0530

    config server setupped.

---
+++ src/test/java/org/books/configService/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.books.configService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
